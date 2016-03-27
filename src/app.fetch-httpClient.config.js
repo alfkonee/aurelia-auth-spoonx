0d7f330eaf2d62c1e95599a535f146008ad296b6
Alfred Neequaye <alfkonee@hotmail.com>
2016-03-27 01:27:50 +0000

    Handle 401 error in response Interceptor

--- src/app.fetch-httpClient.config.js
+++ src/app.fetch-httpClient.config.js
@@ -1,5 +1,6 @@
 import {HttpClient} from 'aurelia-fetch-client';
 import {AuthService} from './authService';
+import {Storage} from './storage';
 import {BaseConfig} from './baseConfig';
 import {inject} from 'aurelia-dependency-injection';
 import {Config, Rest} from 'spoonx/aurelia-api';
@@ -14,11 +15,16 @@ export class FetchConfig {
    * @param {Authentication} authService
    * @param {BaseConfig} config
    */
-  constructor(httpClient, clientConfig, authentication, config) {
+  constructor(httpClient, clientConfig, authentication, storage, config) {
     this.httpClient   = httpClient;
     this.clientConfig = clientConfig;
     this.auth         = authentication;
     this.config       = config.current;
+    this.storage      = storage;
+  }
+
+  get tokenName() {
+    return this.config.tokenPrefix ? `${this.config.tokenPrefix}_${this.config.tokenName}` : this.config.tokenName;
   }
 
   /**
@@ -29,14 +35,16 @@ export class FetchConfig {
   get interceptor() {
     let auth    = this.auth;
     let config  = this.config;
+    let client = this.httpClient;
+    let storage = this.storage;
+    that = this;
 
     return {
       request(request) {
         if (!auth.isAuthenticated() || !config.httpInterceptor) {
           return request;
         }
-
-        let token = auth.getToken();
+        let token = storage.get(that.tokenName);
 
         if (config.authHeader && config.authToken) {
           token = `${config.authToken} ${token}`;
@@ -46,20 +54,39 @@ export class FetchConfig {
 
         return request;
       },
-      response(response) {
-        if (response.ok) {
-          return response;
-        }
-        if (response.status === 401) {
-          if (auth.isTokenExpired() && config.httpInterceptor) {
-            let refreshTokenName = config.refreshTokenPrefix ? `${config.refreshTokenPrefix}_${config.refreshTokenName}` : config.refreshTokenName;
-            if (storage.get(refreshTokenName)) {
-              auth.updateToken();
+      response(response, request) {
+        return new Promise(
+          (resolve, reject) => {
+            if (response.ok) {
+              resolve(response);
             }
-            //TODO: Reissue Request when aurelia-fetch implements it
+            if (response.status === 401) {
+              if (auth.isTokenExpired() && config.httpInterceptor) {
+                let refreshTokenName: string = config.refreshTokenPrefix ? `${config.refreshTokenPrefix}_${config.refreshTokenName}` : config.refreshTokenName;
+                if (storage.get(refreshTokenName)) {
+                  auth.updateToken()
+                    .then(
+                    () => {
+                      let token = storage.get(that.tokenName);
+                      if (config.authHeader && config.authToken) {
+                        token = `${config.authToken} ${token}`;
+                      }
+                      request.headers.append('Authorization', token);
+                      client.fetch(request)
+                        .then(resp => {
+                          resolve(resp);
+                        });
+                    }
+                    ).catch(err => {
+                      reject(err);
+                    });
+                } else {
+                  resolve(response);
+                }
+              }
+            }
           }
-        }
-        return response;
+        );
       }
     };
   }
